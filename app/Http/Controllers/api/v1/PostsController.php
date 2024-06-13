159e66b18c0d26dc7d59e2060e22cf1432ee19b8
“yahyaroot” <amriyahya.email@gmail.com>
2024-06-13 23:06:09 +0800

    learn rest API

---
+++ app/Http/Controllers/api/v1/PostsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+
+
+class PostsController extends Controller
+{
+
+
+
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return response([
+            'success' => true,
+            'message' => 'List Semua Posts',
+            'data' => $posts
+        ], 200);
+    }
+
+
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ],
+            [
+                'title.required' => 'Masukkan Title Post !',
+                'content.required' => 'Masukkan Content Post !',
+            ]
+        );
+
+        if($validator->fails()) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Silahkan Isi Bidang Yang Kosong',
+                'data'    => $validator->errors()
+            ],401);
+
+        } else {
+
+            $post = Post::create([
+                'title'     => $request->input('title'),
+                'content'   => $request->input('content')
+            ]);
+
+            if ($post) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Post Berhasil Disimpan!',
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Post Gagal Disimpan!',
+                ], 401);
+            }
+        }
+    }
+
+    public function show($id)
+    {
+        $post = Post::whereId($id)->first();
+
+
+        if ($post) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Detail Post!',
+                'data'    => $post
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post Tidak Ditemukan!',
+                'data'    => ''
+            ], 401);
+        }
+    }
+    
+
+    }
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\v1\PostsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+Route::get('/v1/posts', [PostsController::class, 'index']);
+Route::post('/v1/posts/store', [PostsController::class, 'store']);
+
+Route::get('/v1/posts/{id?}', [PostsController::class, 'show']);
